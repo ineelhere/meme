3d629fe7bdb8615083461c6a3d9847f33b0b62cc
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2022-01-30 19:54:34 +0530

    Update

--- memescheme.py
+++ memescheme.py
@@ -62,5 +62,11 @@ st.sidebar.info("""
 * Don't use hate speech in the memes.
 """)
 
+st.sidebar.markdown("""
+___
+*Indraneel Chakraborty*
+</p> <h6 class="jumbotron-heading" align = 'center'><br> <a href="https://www.linkedin.com/in/indraneelchakraborty/" target="_blank"><img src="https://img.icons8.com/fluency/48/000000/linkedin.png"/> </a>  <a href="https://twitter.com/ineelhere" target="_blank"> <img src="https://abs.twimg.com/favicons/twitter.ico" alt="Twitter" width="32" height="32"> </a> <a href="https://www.youtube.com/channel/UCbIMzl7rOj0FkamVf_aBM8w" target="_blank"> <img src="https://www.youtube.com/s/desktop/28b67e7f/img/favicon_48.png" alt="YouTube" width="32" height="32"> </a> <a href="https://github.com/ineelhere" target="_blank"><img width="32" height="32" src="https://github.com/fluidicon.png" alt="Github" > </a> </h6><br> <p align=center > <a href="https://docs.google.com/forms/d/e/1FAIpQLSeZuuBTcglrHmKFfTwZ66HdHVYKge6kJ3cAtSCdF7e_8NMypg/viewform" class="btn btn-outline-success" target="_blank"><strong>Contact Me</strong></a> </p> </div> </section> </main>
+""", unsafe_allow_html=True)
+
 if st.checkbox("Show the source for these Memes"):
     sources()
